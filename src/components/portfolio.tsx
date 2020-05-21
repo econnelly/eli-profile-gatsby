0a92af13e318232ab53d6e98e806c53e0f967182
Eli Connelly <eli@eliconnelly.com>
2020-05-21 09:21:28 -0700

    Run prettier

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,41 +1,40 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import Image from 'gatsby-image'
+import Image from "gatsby-image"
 
 export const Portfolio = () => {
   const data = useStaticQuery(graphql`
     query {
-  projects: allProjectsJson {
-    edges {
-      node {
-        url
-        title
-        image
-        fields {
-          portfolioImage {
-            childImageSharp {
-                fluid(maxWidth: 200, maxHeight: 200) {
-                  ...GatsbyImageSharpFluid
+      projects: allProjectsJson {
+        edges {
+          node {
+            url
+            title
+            image
+            fields {
+              portfolioImage {
+                childImageSharp {
+                  fluid(maxWidth: 200, maxHeight: 200) {
+                    ...GatsbyImageSharpFluid
+                  }
                 }
               }
+            }
           }
         }
       }
     }
-  }
-}
-
   `)
 
   const projects = data.projects.edges.map(function (project: any) {
-
     return (
       <div key={project.node.title} className="columns portfolio-item">
         <div className="item-wrap">
           <a href={project.node.url} title={project.node.title}>
-
             {/* See gatsby-node.js for why this image loading works */}
-            <Image fluid={project.node.fields.portfolioImage.childImageSharp.fluid} />
+            <Image
+              fluid={project.node.fields.portfolioImage.childImageSharp.fluid}
+            />
 
             <div className="overlay">
               <div className="portfolio-item-meta">
