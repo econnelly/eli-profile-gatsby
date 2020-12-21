a0606183914929556dba898e0a5bb832b0d62143
Eli <eli@eliconnelly.com>
2020-12-20 21:06:32 -0800

    Apply prettier formatting

--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -1,13 +1,12 @@
 import React from "react"
 import { scheduleSection } from "../js/navids"
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet"
 
 const ScheduleMeeting = () => {
-
   return (
     <section id={scheduleSection}>
       <Helmet>
-        <script src={"https://assets.calendly.com/assets/external/widget.js"}/>
+        <script src={"https://assets.calendly.com/assets/external/widget.js"} />
       </Helmet>
       <div id="schedule_form">
         <div
